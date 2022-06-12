815c32264a7d4e1c01faaf46db54b6d6f23fc8d4
sawagenaar <sadanilova@hotmail.com>
2022-06-12 21:52:46 +0200

    first commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="notFound">
+    <h1>Not Found</h1>
+    <p>
+      Oops, we couldn't find that page. Try going
+      <router-link to="/">Home</router-link>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.notFound {
+  font-size: 20px;
+  margin-top: 3em;
+  text-align: center;
+}
+</style>
